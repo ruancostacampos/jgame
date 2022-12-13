70662b9603c1e35bb7cb56ca3b86113ad0dff59d
Ruan Costa Campos <ruancosta9797@gmail.com>
2022-12-12 22:47:28 -0300

    implementando tiro

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 import PlayerTank from "./PlayerTank.js";
 
+const container = document.getElementById('container')
 const playerTank = new PlayerTank(document.getElementById('playerTank'))
 const EnemyTank = new PlayerTank(document.getElementById('enemyTank'))
 
@@ -45,19 +46,30 @@ function checkKey(e) {
 function shot(player){
     
     if(player.facing = 'top'){
+
+        //Create 'bullet' block
         let newBullet = document.createElement('div')
         newBullet.classList.add('block')
-        document.getElementById('container').appendChild(newBullet)
-        console.log(player.top)
+        //Add to the container
+        container.appendChild(newBullet)
+        //Move to the front of cannon
         newBullet.style.top = `${player.top - 22}px`
         newBullet.style.left = `${player.left + 22}px`
 
-        for(let i; i<6; i++){
-            setInterval( () => {}, 1000)
-        }
+        const colision = setInterval( () => {
+            if( (pxToNumber(newBullet) - 20) > container.offsetLeft){
+                clearInterval
+            }
+            newBullet.style.top = `${newBullet.style.top.replace('px', '') - 20}px`
+        }, 500)
 
     }
     
+}
+
+// Convert string ex: '120px' to 120
+function pxToNumber(elem){
+    return +elem.style.top.replace('px','')
 }
 
 window.requestAnimationFrame(update)
